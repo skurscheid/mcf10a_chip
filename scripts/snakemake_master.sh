3a5584defb004f1941887d7d1e25105ed6715795
Sebastian Kurscheid <sebastian.kurscheid@gmail.com>
2020-06-11 13:22:18 +1000

    makes project-specific parameter changes

--- scripts/snakemake_master.sh
+++ scripts/snakemake_master.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#PBS -P pb97
-#PBS -l walltime=24:00:00
+#PBS -P zi26
+#PBS -l walltime=48:00:00
 #PBS -l wd
-#PBS -q biodev
+#PBS -q express
 #PBS -e /home/150/sxk150/qsub_error
 #PBS -o /home/150/sxk150/qsub_out
 #PBS -l ncpus=4
-#PBS -l mem=4GB
+#PBS -l mem=32GB
 #PBS -M skurscheid@gmail.com
 #PBS -m abe
 #PBS -l storage=scratch/kv78+gdata/kv78
@@ -15,8 +15,8 @@ target=${cli_target}
 
 source ~/.bashrc
 
-/home/150/sxk150/miniconda3/envs/snakemake/bin/snakemake -s /home/150/sxk150/mcf10a_hic/Snakefile ${cli_target}\
-    --configfile /home/150/sxk150/mcf10a_hic/config.yaml\
+/home/150/sxk150/miniconda3/envs/snakemake/bin/snakemake -s /home/150/sxk150/mcf10a_chip/Snakefile ${cli_target}\
+    --configfile /home/150/sxk150/mcf10a_chip/config.yaml\
 	--use-conda\
 	--cluster "qsub -P {cluster.P}\
                     -l ncpus={threads} \
@@ -24,18 +24,18 @@ source ~/.bashrc
                     -l mem={cluster.mem} \
                     -l wd\
                     -l walltime={cluster.walltime}\
-		    -l storage={cluster.storage}\
-		    -l jobfs={cluster.jobfs}\
+		            -l storage={cluster.storage}\
+		            -l jobfs={cluster.jobfs}\
                     -M {cluster.M}\
                     -m {cluster.m}\
                     -e {cluster.error_out_dir} \
                     -o {cluster.std1_out_dir}" \
 	--jobs 100\
-	-d ~/data/mcf10a-hic\
+	-d /home/150/sxk150/data/mcf10a-hic/ChIP\
 	--rerun-incomplete \
     --local-cores 1\
-	--cluster-config /home/150/sxk150/mcf10a_hic/cluster.json\
-    --config rest_enzyme=DpnII_HinfI machine=gadi\
+	--cluster-config /home/150/sxk150/mcf10a_chip/cluster.json\
+    --config machine=gadi\
     --keep-going\
 	-pr 
 
