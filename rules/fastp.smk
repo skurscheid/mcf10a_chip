5ec3470fd643908bb00486ea133b085011e45de2
Sebastian Kurscheid <sebastian.kurscheid@gmail.com>
2020-06-11 12:47:54 +1000

    still trying to fix issue with worfklow

--- rules/fastp.smk
+++ rules/fastp.smk
@@ -21,7 +21,7 @@ rule run_fastp_pe:
     log:
         log = "logs/fastp/pe/{biosample}/{library_type}/{replicate}/{run}.log"
     input:
-        unpack(fastp_input)
+        fastp_input
     output:
         out1 = "fastp/trimmed/pe/{biosample}/{library_type}/{replicate}/{run}_1.fastq.gz",
         out2 = "fastp/trimmed/pe/{biosample}/{library_type}/{replicate}/{run}_2.fastq.gz",
@@ -29,7 +29,7 @@ rule run_fastp_pe:
         report_json = "fastp/report/pe/{biosample}/{library_type}/{replicate}/{run}.fastp.json"
     shell:
         """
-            fastp -i {input.fq1} -I {input.fq2}\
+            fastp -i {input[0]} -I {input[1]}\
                   -o {output.out1} -O {output.out2}\
                   --html {output.report_html} --json {output.report_json}\
                   --length_required 30\
--- scripts/helper.py
+++ scripts/helper.py
@@ -22,11 +22,6 @@ def create_testing_input(base_path, units):
         fq2.touch(exist_ok = True)
 
 # functions for fastp rules
-def fastp_input(runTable, wildcards):
+def fastp_input(wildcards, runTable):
     """function for creating gathering input files for fastp processing"""
-    t = {}
-    row = runTable.loc[(runTable.Run == wildcards['Run']) & (runTable.BioSample == wildcards['BioSample']) & (runTable.replicate == wildcards['rep']) & (runTable.library_type == wildcards['library_type'])]
-    fq1, fq2 = row['fq1'].to_string(index=False), row['fq2'].to_string(index=False)
-    t['fq1'] = 'raw/' + fq1.strip()
-    t['fq2'] = 'raw/' + fq2.strip()
-    return(t)
+    return(runTable.loc[(runTable.Run == wildcards['Run']) & (runTable.BioSample == wildcards['BioSample']) & (runTable.replicate == wildcards['rep']) & (runTable.library_type == wildcards['library_type']), ['fq1', 'fq2']].values.tolist())
